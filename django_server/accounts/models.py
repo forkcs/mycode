bc3c84758762ca6ed8711d305c06fcedbc533cdb
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-10-30 22:48:04 +0300

    Write Account model

--- django_server/accounts/models.py
+++ django_server/accounts/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+ACCOUNT_TYPES = (
+    (1, 'Admin'),
+    (2, 'Teacher'),
+    (3, 'Student')
+)
+
+
+class Account(models.Model):
+    class Meta:
+        verbose_name = 'Account'
+        verbose_name_plural = 'Accounts'
+
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    account_type = models.PositiveSmallIntegerField(choices=ACCOUNT_TYPES)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    school_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        pass
