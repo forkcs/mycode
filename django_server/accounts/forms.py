ada20d598f67d4ba42e661baa42e72904aa4d395
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-10-30 23:32:11 +0300

    Write views and forms for auth

--- django_server/accounts/forms.py
+++ django_server/accounts/forms.py
@@ -1,4 +1,30 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.forms import ModelForm, CharField, ValidationError
+
+from django_server.accounts.models import Account
+
+User = get_user_model()
 
 LoginForm = AuthenticationForm
-RegisterForm = UserCreationForm
+
+
+class RegisterForm(ModelForm):
+    class Meta:
+        fields = '__all__'
+
+    username = UsernameField()
+    password1 = CharField(min_length=8, required=True)
+    password2 = CharField(min_length=8, required=True)
+
+    def save(self, commit=True):
+        password = self.cleaned_data.pop('password1')
+        if password != self.cleaned_data.pop('password2'):
+            raise ValidationError('Пароли должны совпадать!')
+        username = self.cleaned_data.pop('username')
+
+        new_user = User.objects.create(username=username)
+        new_user.set_password(password)
+        new_user.save()
+
+        Account.objects.create(**self.cleaned_data)
--- django_server/accounts/views.py
+++ django_server/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import FormView
 
-# Create your views here.
+from django_server.accounts.forms import RegisterForm
+
+
+class RegisterView(FormView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    success_url = ''
+
+
+class LogInView(LoginView):
+    template_name = 'accounts/login.html'
+
+
+class LogOutView(LogoutView):
+    pass
