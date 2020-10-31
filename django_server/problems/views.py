b7f5ac489cce8a6542074c1bbeb92c5cac5db984
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-10-31 22:48:59 +0300

    Fix url kwargs in problem detail view

--- django_server/problems/views.py
+++ django_server/problems/views.py
@@ -6,6 +6,5 @@ from django_server.problems.models import Problem, Solution
 class ProblemDetailView(DetailView):
     context_object_name = 'problem'
     model = Problem
-    pk_url_kwarg = 'id'
 
     template_name = 'problems/problem.html'
