37cfbe0f3635bfa044135b0ff61c9323303ddef4
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-10-31 14:50:30 +0300

    Add volume for db_data

--- django_server/django_root/settings.py
+++ django_server/django_root/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'django_root.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent / 'db_data/' / 'db.sqlite3',
     }
 }
 
