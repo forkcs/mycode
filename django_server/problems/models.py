7ce21003f3b7b885a0eb4ab0bc0146a48c7b25e0
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-10-31 16:57:23 +0300

    Write core models
    Add Problem and Solution

--- django_server/problems/models.py
+++ django_server/problems/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from django_server.accounts.models import Account
+
+LANGUAGES = (
+    (1, 'Python'),
+    (2, 'C++'),
+)
+
+
+class Problem(models.Model):
+    class Meta:
+        verbose_name = "Problem"
+        verbose_name_plural = "Problems"
+
+    teacher = models.ForeignKey(to=Account, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=100_000)
+    language = models.SmallIntegerField(choices=LANGUAGES)
+    tests = models.CharField(max_length=1000)
+
+    def __str__(self):
+        pass
+
+
+PROBLEM_STATUSES = (
+    (1, 'OK'),
+    (2, 'Error'),
+    (3, 'Pending'),
+    (4, 'No Status')
+)
+
+
+class Solution(models.Model):
+    class Meta:
+        verbose_name = "Solution"
+        verbose_name_plural = "Solutions"
+
+    student = models.ForeignKey(to=Account, on_delete=models.CASCADE)
+    problem = models.ForeignKey(to=Problem, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100_000)
+    status = models.SmallIntegerField(choices=PROBLEM_STATUSES, null=True, blank=True, default=4)
+    score = models.PositiveIntegerField(null=True, blank=True, default=0)
+
+    def __str__(self):
+        pass
