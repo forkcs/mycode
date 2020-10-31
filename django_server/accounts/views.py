4aa2ee02b0aae091aa60eba2114193f6cbae4012
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-10-31 17:45:10 +0300

    Fix success url in register view

--- django_server/accounts/views.py
+++ django_server/accounts/views.py
@@ -7,7 +7,7 @@ from django_server.accounts.forms import RegisterForm
 class RegisterView(FormView):
     form_class = RegisterForm
     template_name = 'accounts/register.html'
-    success_url = 'admin/'
+    success_url = '/admin/'
 
 
 class LogInView(LoginView):
